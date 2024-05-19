ba5c21b301ec4265531c9f939d66e2d906371ef7
naresh <nareshpogiri409@gmail.com>
2024-05-19 20:10:45 +0530

    "port changed"

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const fs = require('fs').promises;
 const cors = require('cors');
 const app = express();
-const PORT = 5000;
+const PORT = 5001;
 const Path = require('path');
 
 express.static('client')
